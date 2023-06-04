98b65d05862d019aaeecdde6f946250d0f212742
Sarthak Tomar <sarthaktomargenius@gmail.com>
2023-06-04 22:59:45 +0530

    first commit

---
+++ javascript.js
@@ -0,0 +1,38 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("You Must Write Something!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li); 
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7"             //just a cross symbol
+        li.appendChild(span);
+    }
+    inputBox.value = " ";
+    saveTask();
+}
+ listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");  
+        saveTask(); 
+    }
+    else if(e.target.tagName === "SPAN" ){
+        e.target.parentElement.remove();
+        saveTask();
+    }
+ }, false)
+
+ function saveTask(){
+    localStorage.setItem("data", listContainer.innerHTML);
+
+ }
+
+ function displayTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+ }
+ displayTask();
